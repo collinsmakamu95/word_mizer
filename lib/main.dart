41625161b076220f228b73926b1ce78b6282d8fa
collinsmakamu95 <collinsmakamu95@gmail.com>
2023-01-13 19:04:12 +0300

    Favourites page added

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = Favourites();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -150,6 +150,36 @@ class GeneratorPage extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class Favourites extends StatelessWidget {
+  const Favourites({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
